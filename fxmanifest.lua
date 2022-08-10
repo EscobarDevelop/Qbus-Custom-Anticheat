3803f1773d33fda73c9ed15b619e8fc6b2d05fdc
Dalibor <111007918+EscobarDevelop@users.noreply.github.com>
2022-08-10 20:44:58 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+game 'gta5'
+
+version '2.0.0'
+author 'Escobar#4682'
+description 'Anti-cheat for QB-Core'
+
+client_script {
+	'configs/*.lua',
+	'tables/*.lua',
+	'client.lua'
+}
+
+server_scripts {
+	'configs/*.lua',
+	'tables/*.lua',
+	'server.lua'
+}
+
